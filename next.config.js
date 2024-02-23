8e5e25f43ddde94fa3827004108c73bfc3c3b53f
parus <parus25_11q@mail.ru>
2024-02-23 19:39:20 +1000

    dsdd

--- next.config.js
+++ next.config.js
@@ -9,16 +9,6 @@ const nextConfig = {
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true,
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      // Add a favicon plugin
-      config.plugins.push(
-        new webpack.DefinePlugin({
-          'process.env.FAVICON': JSON.stringify('images/favicon.svg'),
-        })
-      );
-
-      return config;
-    },
   },
 };
 
