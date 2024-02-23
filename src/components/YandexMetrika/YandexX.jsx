25781726f56068b0fc08c1f46b52d4daa3824655
swu <parus25_11q@mail.ru>
2024-02-23 16:34:56 +1000

    q

---
+++ src/components/YandexMetrika/YandexX.jsx
@@ -0,0 +1,19 @@
+// app/components/YandexMetrika/YandexX.jsx
+"use client";
+import Image from "next/image";
+
+const YandexX = () => {
+  return (
+    <a href="https://webmaster.yandex.ru/siteinfo/?site=https://vv.sunwithus.ru">
+      <Image
+        width="88"
+        height="31"
+        alt=""
+        border="0"
+        src="https://yandex.ru/cycounter?https://vv.sunwithus.ru&theme=light&lang=ru"
+      />
+    </a>
+  );
+};
+
+export default YandexX;
