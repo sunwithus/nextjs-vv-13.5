3f6388f26f03caf22c799f5ead8d3d1708425a0c
swu <parus25_11q@mail.ru>
2024-03-31 12:34:12 +1000

    db accords + bg-fixed

--- src/app/db/page.tsx
+++ src/app/db/page.tsx
@@ -1,50 +1,44 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
 import Chord from "@tombatossals/react-chords/lib/Chord";
 import guitarData from "./guitar.json";
 
 const instrument = guitarData.main;
 instrument.tunings = guitarData.tunings;
 
-// Функциональный компонент для отображения одного аккорда
-const MyChord = () => {
-  const chord = guitarData.chords.C[0].positions[0];
-  const lite = false; // defaults to false if omitted
-  return <Chord chord={chord} instrument={instrument} lite={lite} />;
-};
-
-// Функциональный компонент для отображения списка аккордов
 const ChordList = () => {
   const chords = guitarData.chords;
   const allChords = Object.keys(chords);
-  /* //////
-  const object1 = {
-  a: 'somestring',
-  b: 42,
-  c: false,
-};
-
-  console.log(Object.keys(object1));
-  // Expected output: Array ["a", "b", "c"]
-  */
 
   return (
-    <div className="flex flex-row">
-      {allChords.map((chordName) => (
-        <Chord
-          key={chordName}
-          chord={chords[chordName][0].positions[0]}
-          instrument={instrument}
-        />
-      ))}
+    <div className="flex flex-row flex-wrap flex-1">
+      {allChords.map((chordName) =>
+        chords[chordName].map((chord, index) => (
+          <>
+            <div className="m-auto">
+              {chord.key +
+                (chord.suffix === "minor"
+                  ? "m"
+                  : chord.suffix === "major"
+                  ? ""
+                  : chord.suffix)}
+            </div>
+            <Chord
+              className=" max-w-10"
+              key={index}
+              chord={chord.positions[0]}
+              instrument={instrument}
+            />
+          </>
+        ))
+      )}
     </div>
   );
 };
 
-export default function DB() {
+export default function GuitarChords() {
   return (
     <>
+      <h1>Guitar Chords</h1>
       <div>
         <ChordList />
       </div>
