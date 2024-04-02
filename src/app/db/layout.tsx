879598f17e8a22a06f184df62aa8e05f75829284
parus <parus25_11q@mail.ru>
2024-04-03 01:34:45 +1000

    dfg

--- src/app/db/layout.tsx
+++ src/app/db/layout.tsx
@@ -1,9 +1,34 @@
-import type { PropsWithChildren } from "react";
+import type { PropsWithChildren } from 'react';
+import './style.css';
 
 export default function NewsLayout({ children }: PropsWithChildren<unknown>) {
   return (
     <>
-      <div className="flex">{children}</div>
+      <div className="mt-24"> </div>
+      <div className="container">
+        <h1>Guitar Chords</h1>
+        <ul className="pl-3 flex flex-wrap">
+          <li className="font-bold p-1">Keys:</li>
+          <li className="mr-3">
+            <a className="bg-blue-500 text-white hover:bg-blue-500 hover:border-blue-200 inline-block border font-bold rounded py-1 px-3" href="/react-chords/guitar" aria-current="page">
+              All
+            </a>
+          </li>
+          <li className="mr-3">
+            <a className="border-white text-blue-500 hover:border-gray-200 hover:bg-gray-200 inline-block border font-bold rounded py-1 px-3" href="/react-chords/guitar/C">
+              C
+            </a>
+          </li>
+          <li className="mr-3">
+            <a className="border-white text-blue-500 hover:border-gray-200 hover:bg-gray-200 inline-block border font-bold rounded py-1 px-3" href="/react-chords/guitar/Csharp">
+              C#
+            </a>
+          </li>
+        </ul>
+        <div className="w-full">
+          <div className="litegrid">{children}</div>
+        </div>
+      </div>
     </>
   );
 }
--- src/app/db/page.tsx
+++ src/app/db/page.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import Chord from "@tombatossals/react-chords/lib/Chord";
-import guitarData from "./guitar.json";
+import React from 'react';
+import Link from 'next/link';
+import Chord from '@tombatossals/react-chords/lib/Chord';
+import guitarData from './guitar.json';
 
 const instrument = guitarData.main;
 instrument.tunings = guitarData.tunings;
@@ -10,35 +11,23 @@ const ChordList = () => {
   const allChords = Object.keys(chords);
 
   return (
-    <div className="flex flex-row flex-wrap flex-1 w-48">
-      {allChords.map((chordName) => /* */
+    <>
+      {allChords.map((chordName /* */) =>
         chords[chordName].map((chord, index) => (
-          <>
-            <div className="m-auto">
-              {chord.key +
-                (chord.suffix === "minor"
-                  ? "m"
-                  : chord.suffix === "major"
-                  ? ""
-                  : chord.suffix)}
-            </div>
-            <Chord
-              className=" max-w-10"
-              key={index}
-              chord={chord.positions[0]}
-              instrument={instrument}
-            />
-          </>
+          // eslint-disable-next-line react/jsx-key
+          <Link href={`/react-chords/guitar/${chord.key}/${chord.suffix}`}>
+            <div className="text-lg text-center m-auto">{chord.key + (chord.suffix === 'minor' ? 'm' : chord.suffix === 'major' ? '' : chord.suffix)}</div>
+            <Chord className=" max-w-10" key={index} chord={chord.positions[0]} instrument={instrument} />
+          </Link>
         ))
       )}
-    </div>
+    </>
   );
 };
 
 export default function GuitarChords() {
   return (
     <>
-      <h1>Guitar Chords</h1>
       <div>
         <ChordList />
       </div>
