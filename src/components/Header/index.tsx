23fd6f6524154ff2a8c9f71df3ff0d3598d91967
swu <parus25_11q@mail.ru>
2024-04-09 01:48:41 +1000

    active links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,28 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
-import { Disclosure } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Disclosure } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
-import { useState } from "react";
+import { useState } from 'react';
+
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 // Функция для объединения классов
 function classNames(...classes: any) {
-  return classes.filter(Boolean).join(" ");
+  return classes.filter(Boolean).join(' ');
 }
 
 // Компонент Header
 const Header = () => {
   // Массив объектов для навигации
   const [navigation, setNavigation] = useState([
-    { name: "Аккорды", href: "/accords", current: false },
-    { name: "Новости", href: "/news", current: false },
-    { name: "Биография", href: "/biography", current: false },
-    { name: "Фильмы", href: "/films", current: false },
-    { name: "db Accords", href: "/db", current: false },
+    { name: 'Аккорды', href: '/accords', current: false },
+    { name: 'Новости', href: '/news', current: false },
+    { name: 'Биография', href: '/biography', current: false },
+    { name: 'Фильмы', href: '/films', current: false },
+    { name: 'db Accords', href: '/db', current: false },
   ]);
 
   // Функция для обновления текущего элемента в навигации
@@ -43,6 +46,7 @@ const Header = () => {
     setNavigation(updatedNavigation);
   };
 
+  const pathname = usePathname();
   return (
     <Disclosure as="nav" className="bg-black fixed z-50 min-w-full">
       {({ open }) => (
@@ -54,11 +58,7 @@ const Header = () => {
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Меню</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
+                  {open ? <XMarkIcon className="block h-6 w-6" aria-hidden="true" /> : <Bars3Icon className="block h-6 w-6" aria-hidden="true" />}
                 </Disclosure.Button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
@@ -69,13 +69,7 @@ const Header = () => {
                       handleHomeClick();
                     }}
                   >
-                    <Image
-                      width="32"
-                      height="32"
-                      className="h-8 w-auto"
-                      src="/images/logo/logo2.png"
-                      alt=""
-                    />
+                    <Image width="32" height="32" className="h-8 w-auto" src="/images/logo/logo2.png" alt="" />
                   </Link>
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
@@ -85,13 +79,11 @@ const Header = () => {
                       <Link
                         key={item.name}
                         href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-800 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "rounded-md px-3 py-2 text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
+                        className={clsx('text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium', {
+                          //'bg-gray-600 text-white': pathname === item.href + '/',
+                          'bg-gray-700 text-white': pathname.startsWith(item.href),
+                        })}
+                        aria-current={item.current ? 'page' : undefined}
                         onClick={() => {
                           handleNavigationClick(index);
                         }}
@@ -105,13 +97,7 @@ const Header = () => {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 {/* Дропдаун профиля */}
                 <Link href="https://vk.com/vvsunwithusru" target="_blank">
-                  <Image
-                    width={32}
-                    height={32}
-                    className="h-8 w-8 rounded-full"
-                    src="/images/vk.png"
-                    alt=""
-                  />
+                  <Image width={32} height={32} className="h-8 w-8 rounded-full" src="/images/vk.png" alt="" />
                 </Link>
               </div>
             </div>
@@ -122,18 +108,7 @@ const Header = () => {
             <div className="space-y-1 px-2 pb-3 pt-2">
               {/* Навигационные кнопки для мобильного меню */}
               {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-1000 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
+                <Disclosure.Button key={item.name} as="a" href={item.href} className={classNames(item.current ? 'bg-gray-1000 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium')} aria-current={item.current ? 'page' : undefined}>
                   {item.name}
                 </Disclosure.Button>
               ))}
